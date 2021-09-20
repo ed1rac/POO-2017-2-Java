eaf3965e7d65c3e0f46ef919fcab04c64a13f766
ed1rac <edkallenn@gmail.com>
2021-09-20 01:13:37 -0500

    Pequenas alterações

--- src/aula04/CalculaRaizQuadrada.java
+++ src/aula04/CalculaRaizQuadrada.java
@@ -11,8 +11,8 @@ public class CalculaRaizQuadrada {
 		
 		System.out.println("Programa que calcula a raiz quadrada de 2");
 		//double raizde2 = Math.sqrt(2);
-		System.out.println("A raiz quadrada de 2 é: " + CalculaRaizQuadrada.raiz2(2) );
-		System.out.println("O valor de PI é: " + Math.PI);
+		System.out.println("A raiz quadrada de 2 Ã©: " + CalculaRaizQuadrada.raiz2(2) );
+		System.out.println("O valor de PI Ã©: " + Math.PI);
 		System.out.println(Math.pow(2, 10));
 		for(int i=1;i<=20;i++){
 			System.out.println("2 ^ " + i + " = " + Math.pow(2, i));
